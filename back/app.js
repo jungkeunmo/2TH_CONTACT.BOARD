e162346ed16a6d968b25c1508a5e020276ff076a
정근모 <junggeunmo@jeong-geunmoui-MacBookAir.local>
2022-04-02 10:01:54 +0900

    board

--- back/app.js
+++ back/app.js
@@ -3,12 +3,159 @@ const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const db = require("./db");
 
 const PORT = 4000;
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.post("/test/seed/database", (req, res) => {
+    const title = [
+        "테스트1",
+        "테스트2",
+        "테스트3",
+        "테스트4",
+        "테스트5",
+
+        "테스트6",
+        "테스트7",
+        "테스트8", 
+        "테스트9",
+        "테스트10",
+    ];
+    const content = [];
+    const author = [
+        "익명1",
+        "익명2",
+        "익명3",
+        "익명4",
+        "익명5",
+
+        "익명6",
+        "익명7",
+        "익명8",
+        "익명9",
+        "익명10",
+    ];
+    const pass = [
+        "1111",
+        "2222",
+        "3333",
+        "4444",
+        "5555",
+
+        "6666",
+        "7777",
+        "8888",
+        "9999",
+        "1000",
+    ];
+
+    // 7번씩 넣기 
+    //랜덤하게 데터 넣기
+
+    for(let i=0; i < 7; i++) {
+        const ran1 = Math.floor(Math.random() * title.length);
+        const ran2 = Math.floor(Math.random() * author.length);
+        const ran3 = Math.floor(Math.random() * pass.length);
+
+        const _title = title[ran1];
+        const _author = author[ran2];
+        const _pass = pass[ran3];
+
+        const insertQuery = `
+            INSERT INTO board (title, content, author, pass, createdAt) VALUES
+            ("${_title}", "SEED CONTENT", "${_author}", "${_pass}", now())
+        `;
+    
+        db.query(insertQuery, (err, rows) => {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log("database Create")
+        });
+    }
+
+    return res.send("Create New Data")
+});
+
+// 1. 쿼리 작성
+// 2. Network Protocal 작성
+// 3. 프론트 개발
+
+app.get("/api/list", (req, res) => {
+    const selectQuery = `
+    SELECT id,
+           title,
+           author,
+           DATE_FORMAT(createdAt, "%Y.%m.%d") AS formatCreatedAt,
+           content,
+           createdAt,
+           hit  
+    FROM  board
+    ORDER BY createdAt DESC
+    `;
+
+    db.query(selectQuery, (error, rows) => {
+        if (error) {
+            console.log(error);
+        }
+
+        return res.status(200).json(rows);
+    });
+}); 
+
+app.post("/api/write", (req, res) => {
+    const {title, author, pass, content} = req.body;
+
+    const insertQuery = `
+        INSERT  INTO    board (
+            title,
+            author,
+            pass,
+            content,
+            createdAt
+        ) VALUES (
+            "${title}",
+            "${author}",
+            "${pass}",
+            "${content}",
+            now()
+        )
+    `;
+
+    db.query(insertQuery, (error, result) => {
+        if(error) {
+            console.log(error);
+            return;
+        }
+
+        return res.status(201).send("게시글에 등록성공")
+    });
+});
+
+app.post("/api/delete", (req, res) => {
+    const { selectId } = req.body;
+
+    const deleteQuery = `
+        DELETE  FROM board
+         WHERE  id = ${selectId}
+    `;
+
+    db.query(deleteQuery, (error, result) => {
+        if(error) {
+            console.log(error);
+            return;
+        }
+
+        return res.status(200).send("게시글 삭제성공")
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`${PORT} SERVER START`);
--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from "react";
 import styled from "styled-components";
 import Typist from "react-typist";
-import {Table} from "antd";
+import {Table, Modal, Button, Form, Input, message} from "antd";
+import axios from "axios";
 
 const Whole = styled.section`
     width: 100%;
@@ -26,8 +27,8 @@ const Wrapper = styled.section`
 
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    align-items: ${(props) => props.al  || "center"};
+    justify-content: ${(props) => props.ju  || "center"};
 
     margin-top: ${(props) => props.mt || 0}px;
 `;
@@ -70,7 +71,112 @@ const MyTable = styled(Table)`
     width: 100%;
 `;
 
+const _D_title = styled.div `
+    margin: 10px 0px 40px 0px;
+    font-size: 18px;
+    padding: 0px 3px;
+    border-bottom: 4px solid #d7d7d7;
+`;
+const _D_author = styled.div ``;
+
+const _D_createdAt = styled.div ``;
+
+const _D_content = styled.div `
+    width: 100%;
+    height: 50vh;
+    border-radius: 7px;
+    box-shadow: 0px 0px 5px #d7d7d7;
+    margin-top: 20px;
+    overflow: scroll;
+`;
+
+const _D_deletBtn = styled.button`
+    margin-top: 20px;
+
+    width: 120px;
+    height: 27px;
+    outline: none;
+    border: none;
+    background-color: red;
+    color: #fff;
+    border-radius: 7px;
+
+    cursor: pointer;
+
+    transition: 0.5s;
+    &:hover {
+        background-color: #fff;
+        color: red;
+    }
+`;
+
+const _D_UpdateBtn = styled.button`
+    margin-top: 20px;
+    margin-left: 10px;
+
+    width: 120px;
+    height: 27px;
+    outline: none;
+    border: none;
+    background-color: skyblue;
+    color: #fff;
+    border-radius: 7px;
+
+    cursor: pointer;
+
+    transition: 0.5s;
+    &:hover {
+        background-color: #fff;
+        color: skyblue;
+    }
+`;
+
+const WriteTextArea = styled(Input.TextArea)`
+   resize: none;
+`;
+
+
 const MyWeb = () => {
+    const [boardList, setBoardList] = useState();
+    const [detailModal, setDetailModal] = useState(false);
+    const [writModal, writeModal] = useState(false);
+
+    const [selectId, SelectId] = useState("");
+    const [dTitle, setDTitle] = useState("");
+    const [dAuthor, setDAuthor] = useState("");
+    const [dCreatedAt, setDCreatedAt] = useState("");
+    const [dContent, setDContent] = useState("");
+
+    const writeForm = useRef();
+
+
+    const getList = async() => {
+        const result = await axios.get("http://localhost:4000/api/list");
+
+        setBoardList(result.data)
+    }
+
+    const writeModalToggle =  () => {
+        writeModal((prev) => !prev);
+    }
+
+    const detailModalToggle = () => {
+        setDetailModal((prev) => !prev);
+    };
+    
+    useEffect(() => {
+        getList();
+    }, []);
+
+    const titleClickHandler = (data) => {
+        SelectId(data.id);
+        setDTitle(data.title);
+        setDAuthor(data.author);
+        setDCreatedAt(data.formatCreatedAt);
+        setDContent(data.content);
+        
+        detailModalToggle(); 
+    }
 
     const columns = [
         {
@@ -79,7 +185,7 @@ const MyWeb = () => {
         },
         {
             title : "Title",
-            dataIndex : "title"
+            render : (data) => <div onClick={() => titleClickHandler(data)}>{data.title}</div>, 
         },
         {
             title : "Author",
@@ -87,13 +193,47 @@ const MyWeb = () => {
         },
         {
             title : "CreatedAt",
-            dataIndex : "createdAt"
+            dataIndex : "formatCreatedAt"
         },
         {
             title : "Hit",
             dataIndex : "hit"
         },
     ]
+
+    const writeFormHandler = async(fd) => {
+        const result = await axios.post("http://localhost:4000/api/write", fd);
+
+        if(result.status === 201) {
+           
+            // 모달창 닫기
+            // 모달안에 데이터 초기화
+            // 데이터 불러오기
+
+            message.success("새로운 게시글이 등록되었습니다.");
+            writeModalToggle();
+            writeForm.current.resetFields();
+            getList();
+        } else {
+            message.error("게시글 등록실패");
+        }
+    };
+
+    const deleteHandler = async () => {
+        console.log(selectId);
+
+        const result = await axios.post("http://localhost:4000/api/delete", {
+            selectId,
+        });
+
+        if(result.status === 200) {
+            message.success("새로운 게시글이 삭제되었습니다.");
+            detailModalToggle();
+            getList();
+        } else {
+            message.error("게시글 삭제실패");
+        }
+    };
 
     return (
         <Whole>
@@ -109,9 +249,72 @@ const MyWeb = () => {
             </Wrapper>
 
             {/* BOARD */}
-            <Wrapper mt="100">
-                <MyTable rowKey="id" columns={columns} dataSource={[]} size="small" />
+            <Wrapper al="flex-end">
+                <Button type="primary" size="small" onClick={() => writeModalToggle()}>글작성하기</Button>
             </Wrapper>
+
+            <Wrapper mt="25">
+                <MyTable rowKey="id" columns={columns} dataSource={boardList} size="small" />
+            </Wrapper>
+
+            {/*DETAIL MODAL */}
+            <Modal footer={null} visible={detailModal} onCancel={() => detailModalToggle()} title="게시글 상세보기" width="100%">
+                <_D_title>{dTitle}</_D_title>
+                <_D_author>{dAuthor}</_D_author>
+                <_D_createdAt>{dCreatedAt}</_D_createdAt>
+                <_D_content>{dContent}</_D_content>
+                <_D_deletBtn onClick={() => deleteHandler()}>삭제</_D_deletBtn>
+                <_D_UpdateBtn>수정</_D_UpdateBtn>
+            </Modal>    
+            {/*DETAIL MODAL */}    
+
+            {/*WRITE MODAR */}
+            <Modal footer={null} visible={writModal} onCancel={() => writeModalToggle ()} title="게시글 작성하기" width="100%">
+                <Form ref={writeForm} wrapperCol={{span : 22}} labelCol={{span : 2}} onFinish={writeFormHandler}>
+                    <Form.Item label="제목" name="title" rules={[
+                        {
+                            required : true,
+                            message : "제목은 필수입력 사항입니다.",
+                        },
+                    ]}>
+                        <Input allowClear/>
+                    </Form.Item>
+
+                    <Form.Item label="작성자" name="author" rules={[
+                        {
+                            required : true,
+                            message : "작성은 필수입력 사항입니다.",
+                        },
+                    ]}>
+                        <Input allowClear/>
+                    </Form.Item>
+
+                    <Form.Item label="비밀번호" name="pass" rules={[
+                        {
+                            required : true,
+                            message : "비밀번호는 필수입력 사항입니다.",
+                        },
+                    ]}>
+                        <Input type="password" allowClear maxLength={4} htmlType="number"/>
+                    </Form.Item>
+
+                    <Form.Item label="내용" name="content" rules={[
+                        {
+                            required : true,
+                            message : "내용은 필수입력 사항입니다.",
+                        },
+                    ]}>
+                        <WriteTextArea allowClear rows={10}/>
+                    </Form.Item>
+                    
+                    <Wrapper al="flex-end">
+                        <Button  type="primary" htmlType="submit">
+                            작성
+                        </Button>
+                    </Wrapper>
+                </Form>
+            {/*WRITE MODAR */}    
+            </Modal>
         </Whole>
     )
 }
